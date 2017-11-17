1284e0af1faedd9d9a928d9962f78fd2bba551c6
virovinrom <32594415+virovinrom@users.noreply.github.com>
2017-11-17 15:04:09 +0200

    v3.0

--- src/lv/FourInALine/Mark.java
+++ src/lv/FourInALine/Mark.java
@@ -1,19 +1,14 @@
 package lv.FourInALine;
 
 public enum Mark {
-    X("X"),  O("O"),  EMPTY("_");
+    X("X"), O("O"), EMPTY("_");
     private String value;
-
     Mark(String value) {
         this.value = value;
     }
-    public boolean contains(Mark field) {
-        if (field.equals(EMPTY)) {
-            return false;
-        }
-        return true;
-    }
+
+    @Override
     public String toString() {
-        return this.value;
+        return value;
     }
 }
